49adceca1d43648bdbbaf08a3d294110b32c63f9
Barnabas <szbarna2004@gmail.com>
2024-06-04 14:15:08 +0200

    create job api slice

--- client/src/state/store.js
+++ client/src/state/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authApiSlice } from "./authApiSlice";
 import { authSlice } from "./authSlice";
+import { jobApiSlice } from "./jobApiSlice";
 
 export const store = configureStore({
   reducer: {
     [authSlice.name]: authSlice.reducer,
     [authApiSlice.reducerPath]: authApiSlice.reducer,
+    [jobApiSlice.reducerPath]: jobApiSlice.reducer,
   },
-  middleware: (getDefaultMiddleWare) => getDefaultMiddleWare().concat(authApiSlice.middleware),
+  middleware: (getDefaultMiddleWare) =>
+    getDefaultMiddleWare().concat(authApiSlice.middleware).concat(jobApiSlice.middleware),
 });
