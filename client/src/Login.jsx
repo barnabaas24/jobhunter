8168f0638e130fa29f5880d600f0b155b9d58a80
Barnabas <szbarna2004@gmail.com>
2024-06-06 20:31:43 +0200

    create login page

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,19 +1,43 @@
 import React from "react";
 import { useLoginMutation } from "./state/authApiSlice";
-import { useDispatch } from "react-redux";
-import { login } from "./state/authSlice";
 
 const Login = () => {
-  const [apiLogin] = useLoginMutation();
+  const [login] = useLoginMutation();
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    try {
+      await login({
+        email: e.target.email.value,
+        password: e.target.password.value,
+      });
+      console.log("succesfull login");
+
+      // localStorage.setItem("accessToken", responseData.accessToken);
+      // localStorage.setItem("user", responseData.user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
-    <button
-      onClick={() => {
-        apiLogin({ body: { email: "user1@jobhunter.hu", password: "user1" } });
-      }}
-    >
-      Login
-    </button>
+    <form onSubmit={handleFormSubmit} className="w-1/3 mx-auto">
+      <div className="flex flex-col gap-6">
+        <label className="input input-bordered flex items-center gap-2">
+          Email
+          <input type="text" name="email" className="grow" placeholder="" required />
+        </label>
+        <label className="input input-bordered flex items-center gap-2">
+          Password
+          <input type="password" name="password" className="grow" placeholder="" required />
+        </label>
+      </div>
+      <div className="pt-6">
+        <button type="submit" className="btn">
+          Login
+        </button>
+      </div>
+    </form>
   );
 };
 
