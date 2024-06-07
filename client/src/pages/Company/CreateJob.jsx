cbc77dfbeb05acb738f5f786dd7dd6b4096361ac
Barnabas <szbarna2004@gmail.com>
2024-06-07 18:33:55 +0200

    create job creation component

--- client/src/pages/Company/CreateJob.jsx
+++ client/src/pages/Company/CreateJob.jsx
@@ -1,7 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import SecondaryHeader from "../../components/SecondaryHeader";
+import { useCreateNewJobMutation } from "../../state/api/jobApi";
 
 const CreateJob = () => {
-  return <div>CreateJob</div>;
+  const [jobType, setJobType] = useState("");
+
+  const [createNewJob] = useCreateNewJobMutation();
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    const formData = {
+      company: e.target.company.value,
+      position: e.target.position.value,
+      description: e.target.description.value,
+      salaryFrom: Number.parseInt(e.target.from.value),
+      salaryTo: Number.parseInt(e.target.to.value),
+      type: jobType,
+      city: e.target.city.value,
+      homeOffice: e.target.homeOffice.checked,
+    };
+    console.log(formData);
+
+    try {
+      await createNewJob(formData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //TODO: render error component
+
+  return (
+    <div>
+      <SecondaryHeader>Új hírdetés</SecondaryHeader>
+
+      <div className="w-1/2 p-6 rounded-lg mx-auto mt-12 shadow-lg">
+        <form onSubmit={handleFormSubmit} className=" w-1/2 mx-auto flex flex-col gap-6">
+          <label className="input input-bordered flex items-center gap-2">
+            Cég neve
+            <input type="text" name="company" />
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            Pozíció neve
+            <input type="text" name="position" />
+          </label>
+          <textarea
+            name="description"
+            rows={4}
+            className="textarea textarea-bordered"
+            placeholder="Leírás a munkáról"
+          ></textarea>
+          <div className="flex flex-col gap-3">
+            <div>Fizetési sáv</div>
+            <div className="flex justify-between">
+              <div className="w-1/3 flex">
+                <label className="input input-bordered flex items-center">
+                  <input type="number" name="from" className="w-[100%]" />
+                  <label>tól</label>
+                </label>
+              </div>
+              <div className="w-1/3 flex">
+                <label className="input input-bordered flex items-center">
+                  <input type="number" name="to" className="w-[100%]" />
+                  <label>ig</label>
+                </label>
+              </div>
+            </div>
+          </div>
+          <select
+            name="jobtype"
+            value={jobType}
+            onChange={(e) => setJobType(e.target.value)}
+            className="select select-bordered w-full max-w-xs"
+          >
+            <option value="" disabled>
+              Foglalkoztatás formája
+            </option>
+            <option value="full-time">Teljes állás</option>
+            <option value="part-time">Részmunkaidős</option>
+            <option value="internship">Gyakornok</option>
+          </select>
+          <label className="input input-bordered flex items-center gap-2">
+            Település
+            <input type="text" name="city" />
+          </label>
+          <label className="label cursor-pointer w-[45%]">
+            <span className="label-text">Home Office lehetőség</span>
+            <input type="checkbox" className="checkbox" name="homeOffice" />
+          </label>
+          <div>
+            <button type="submit" className="btn bg-primary hover:bg-accent">
+              Hírdetés létrehozása
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default CreateJob;
