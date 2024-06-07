6a59558fc1ad5c657ce8444d26de2cd2c9261f04
Barnabas <szbarna2004@gmail.com>
2024-06-07 04:07:50 +0200

    create secondary header component

---
+++ client/src/components/SecondaryHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SecondaryHeader = ({ children }) => {
+  return <div className="text-3xl font-bold shadow-md h-14 pl-6">{children}</div>;
+};
+
+export default SecondaryHeader;
