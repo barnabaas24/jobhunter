2211d712ea95347ffd3694cb3dbb290afa0ef2a9
Barnabas <szbarna2004@gmail.com>
2024-06-08 17:21:54 +0200

    create home page

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,17 +1,56 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useGetAllJobsQuery } from "./state/api/jobApi";
+import { useNavigate } from "react-router-dom";
+import SecondaryHeader from "./components/SecondaryHeader";
+import { selectUserRole } from "./state/authSlice";
 
 const Home = () => {
   const { data: jobs } = useGetAllJobsQuery();
-  const authState = useSelector((state) => state.auth);
-  console.log(authState);
+  const userRole = useSelector(selectUserRole);
+  const navigate = useNavigate();
+
+  function handleJobClick(jobId) {
+    navigate(`/jobs/${jobId}`);
+  }
 
   return (
-    <div className="container mx-auto p-4">
-      {jobs?.map((d) => {
-        <div>{d.name}</div>;
-      })}
+    <div>
+      <SecondaryHeader>Főoldal</SecondaryHeader>
+      <div className="container mx-auto w-1/2 mt-10">
+        <div className="overflow-x-auto shadow-lg rounded-lg p-6">
+          <table className="table">
+            <thead>
+              <tr className="flex justify-between">
+                <th>Állás neve</th>
+                <th>Fizetés</th>
+              </tr>
+            </thead>
+            <tbody>
+              {jobs?.map((job) => (
+                <tr
+                  key={job.id}
+                  onClick={userRole === "jobseeker" ? () => handleJobClick(job.id) : null}
+                  className={`flex justify-between ${userRole === "jobseeker" ? "cursor-pointer" : ""}`}
+                >
+                  <td>
+                    <div className="flex flex-col justify-start">
+                      <span className="font-bold">{job.position}</span>
+                      <span>{job.city}</span>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="flex flex-col justify-start">
+                      <span className="font-bold">{job.salaryFrom + "-" + job.salaryTo + " Ft"}</span>
+                      <span>{job.type}</span>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
